24b97f311ab3705db2e67ff29fa4ab5e43fc09da
youssifhegab <51842259+youssifhegab@users.noreply.github.com>
2021-07-27 15:51:39 +0200

    finished login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import LoadingBar from 'react-redux-loading'
+import Dashboard from './Dashboard'
+import QuestionPage from './QuestionPage'
+import NewQuestion from './NewQuestion'
+import LeaderBoard from './LeaderBoard'
+import Login from './Login'
+import Nav from './Nav'
 
-function App() {
-  return (
-    <div className="App">
-      Hi react
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+  render() {
+    return (
+      <Router>
+        <Fragment>
+          <LoadingBar style={{ backgroundColor: 'red' }} />
+          <div className='container'>
+            <Nav />
+            {this.props.loading === true
+              ? <Login />
+              : <div>
+                <Route path='/' exact component={Login} />
+                <Route path='/dashboard' exact component={Dashboard} />
+                <Route path='/question/:id' component={QuestionPage} />
+                <Route path='/add' component={NewQuestion} />
+                <Route path='/leaderboard' component={LeaderBoard} />
+                </div>}
+          </div>
+        </Fragment>
+      </Router>
+    )
+  }
 }
 
-export default App;
+function mapStateToProps ({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
---
+++ src/components/Login.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Form, FormGroup, Button, Input } from 'reactstrap'
+import { setAuthedUser } from '../actions/autheduser'
+import { Redirect } from 'react-router-dom'
+
+class Login extends Component {
+  state = {
+    user : this.props.authedUser ? this.props.authedUser : '',
+    userAlreadyLog : false,
+    toHome : false
+  }
+  handleSelectProfile = (e) => {
+    const user = e.target.value
+    this.setState(() => ({
+      user
+    }))
+  }
+  handleSubmit = (e) => {
+    const user = this.state.user
+    const { dispatch } = this.props
+    dispatch(setAuthedUser(user))
+    this.setState(() => ({
+      userAlreadyLog : true,
+      toHome : true,
+    }))
+  }
+  render () {
+    console.log('ici', this.state.toHome)
+    if (this.state.toHome === true) {
+      return <Redirect to='/dashboard'/>
+    }
+    return (
+      <div className='center'>
+        <h3>Choose your profile : </h3>
+        <Form >
+          <FormGroup>
+            <Input type="select" value = {this.state.user} onChange={this.handleSelectProfile}>
+            <option value="" >Select your profile</option> 
+              {Object.keys(this.props.users).map((user) => (
+                <option key = {this.props.users[user].id} value = {this.props.users[user].id}>
+                  {this.props.users[user].id}
+                </option>
+              ))}
+            </Input>
+          </FormGroup>
+            <Button
+              className='btn'
+              disabled={this.state.user === ''}
+              onClick={this.handleSubmit}>
+                Log in
+            </Button>
+        </Form>
+      </div>
+    )
+  }
+}
+
+
+
+const mapStateToProps =  ({users, authedUser})=> {
+  return {
+    users,
+    authedUser : authedUser 
+    ? authedUser
+    : null,
+  }
+}
+
+export default connect(mapStateToProps)(Login)
